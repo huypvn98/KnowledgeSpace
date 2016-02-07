e5d1e0a7e813269668223503408ee2237fb3b75e
huypvn98 <huypvn1998@gmail.com>
2016-02-07 15:40:40 +0700

    Temp

--- admin-app/src/app/shared/services/auth.service.ts
+++ admin-app/src/app/shared/services/auth.service.ts
@@ -52,9 +52,8 @@ export class AuthService extends BaseService {
   }
 
   get profile(): Profile {
-    return this.user != null ? this.user.profile : { userName: '', role: '', permissions: [] };
+    return this.user != null ? this.user.profile : null;
   }
-
   async signout() {
     await this.manager.signoutRedirect();
   }
