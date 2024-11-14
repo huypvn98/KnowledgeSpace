b64f939a2bb26acd7f0703651967c49561d8b2eb
huypvn98 <huypvn1998@gmail.com>
2024-11-14 15:20:34 +0700

    Cập nhật folder dist

--- admin-app/src/environments/environment.prod.ts
+++ admin-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://kbapi.tedu.com.vn',
-  authorityUrl: 'https://kbapi.tedu.com.vn',
+  apiUrl: 'https://kbapi.gptstockone.com.local',
+  authorityUrl: 'https://kbapi.gptstockone.com.local',
   clientId: 'angular_admin',
-  adminUrl: 'https://kbadmin.tedu.com.vn'
+  adminUrl: 'https://kbadmin.gptstockone.com.local'
 };
